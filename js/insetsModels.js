66dcc8f22b1e1338ab989b2390805c5c41c9b95f
dimafr97 <dimafr@vk.com>
2026-02-26 23:01:04 +0300

    Update insetsModels.js

--- js/insetsModels.js
+++ js/insetsModels.js
@@ -11,7 +11,7 @@ const RAW_INSETS = [
   desc: "Композиция пересекающихся примитивов",
 
   // путь в защищённом API (после ?path=)
-  sourcePath: "models/1.gltf",
+  sourcePath: "models/2.gltf",
 
   // материал тела (управляется ползунком)
   opacityMaterialName: "1",
